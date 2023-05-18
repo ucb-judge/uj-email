f8c98876faedcdec91bce46f61b3df9eac053a3f
Chris Apaza <chrisalanapazaa@gmail.com>
2023-05-18 02:55:26 -0400

    added dependencies

---
+++ src/main/kotlin/ucb/judge/ujemail/UjEmailApplication.kt
@@ -0,0 +1,11 @@
+package ucb.judge.ujemail
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjEmailApplication
+
+fun main(args: Array<String>) {
+	runApplication<UjEmailApplication>(*args)
+}
---
+++ src/test/kotlin/ucb/judge/ujemail/UjEmailApplicationTests.kt
@@ -0,0 +1,13 @@
+package ucb.judge.ujemail
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UjEmailApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
